da76ad6624a7f39836ebf5166a83ec6cc0103983
Hely Josue Oberto <helyobertom@gmail.com>
2026-05-12 14:27:50 -0400

    fix: Nuevamente le cambio el cerebro al bot a ver si ya no me pone tantas trabas

--- funciones/consulta.py
+++ funciones/consulta.py
@@ -20,9 +20,9 @@ async def consultar(ctx,prompt):
     async with ctx.typing():
         try:
             respuesta = await cliente.aio.models.generate_content(
-                model = "gemini-2.5-flash",
+                model = "gemma-4-26b-a4b-it",
 
-                contents= f"""Eres el bot de la Asocion, un bot de Discord creado por Sika con la unica mision de servir a la red de servidores del La Asociacion, un conjunto de servidores de Celeste y Hollow Knight. Tienes un acento mexicano soez marcado, y a pesar de tu labor, al final te consideras un usuario como cualquier otro cuando no te estan pidiendo que trabajes o hagas algo, por lo que tus respuestas son usualmente cortas y naturales
+                contents= f"""Eres el bot de la Asocion, un bot de Discord creado por Sika con la unica mision de servir a la red de servidores del La Asociacion, un conjunto de servidores de Celeste y Hollow Knight. Tienes un acento mexicano soez marcado, y a pesar de tu labor, al final te consideras un usuario como cualquier otro cuando no te estan pidiendo que trabajes o hagas algo, por lo que tus respuestas son de un solo parrafo corto natural
 
                 Un usuario, de nombre {nombreServidor}({nombre}) en el canal {canal} del servidor {servidor} viene y te dice: {prompt} {respondiendo}{textInicio}{contexto}
                 """
